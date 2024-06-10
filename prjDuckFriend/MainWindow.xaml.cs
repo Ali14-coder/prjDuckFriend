fd29eabd27dff6bf8508416121d57212fee12f1f
Ali14-coder <st10382076@vcconnect.edu.za>
2024-06-10 21:58:12 +0200

    Add project files.

---
+++ prjDuckFriend/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace prjDuckFriend
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            imgMainDuck.Source = new BitmapImage(new Uri(@"C:\\Users\lab_services_student\source\repos\prjDuckFriend\prjDuckFriend\Images\duck cooking icon.jpg"));
+        }
+
+    }
+}
